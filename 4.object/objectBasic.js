3a2a29e11a71c398994ea92dbfee601a492c643d
MC <dhy04057@likelion.org>
2019-09-09 22:56:16 +0900

    third commit

--- 4.object/objectBasic.js
+++ 4.object/objectBasic.js
@@ -1,19 +1,19 @@
 // object에는 각 value에 이름(key)을 줄 수 있다.
-// 파이썬의 딕셔너리와 유사 (key-value 형태의 property를 저장)
+// js의 객체는 파이썬의 딕셔너리와 유사 (key-value 형태의 property를 저장)
 const emptyObject = {}; // 빈 객체 생성
 
 const likeLion = {
-  name: "cbnu",
+  name: "cbnu", // key:value형태의 하나의 property
   op: 6,
   numMembers: 24,
   averageOfStudentID: 2016,
   duHeeLee: true,
-  opName: ["윤창식", "김보연", "이민철", "정아현", "박종현", "구경민"],
+  opName: ["김보연", "이민철", "최유진", "정아현", "박종현", "구경민"],
   // 객체 안에 배열을 생성해도 됨.
 
   opInfo: [
     { name: "mc", age: 22, dept: "isToSwe", gf: false },
-    { name: "yk", age: 27, dept: "agecPlusCs", gf: "iDonKnow" }
+    { name: "yk", age: 27, dept: "agecPlusCs", gf: "iDon'tKnow" }
   ] // 객체 안의 배열 안의 객체
 };
 
@@ -22,7 +22,7 @@ const likeLion = {
 // console.log(likeLion);
 
 //마침표(.)를 사용하여 프로퍼티의 Key를 명시하면, Value를 읽을 수 있다.
-//게다가 이를 활용하여 변경할 수도 있고, 변경할 내용이 없으면 자동으로 신규 생성된다.
+//이를 활용하여 변경할 수도 있고, 변경할 내용이 없으면 자동으로 신규 생성된다.
 
 //likeLion.op = 3; // const로 선언된 변수의 객체의 항목을 바꾸는 것은 가능.
 // console.log(likeLion.op);
--- 5.function/functionMixed.js
+++ 5.function/functionMixed.js
@@ -1,4 +1,5 @@
 //함수 기능을 객체 안에서 구현
+// 참고 : 함수 기능이 객체안에서 구현되었을 경우 이를 '메소드'라고 부른다.
 const calculator = {
   plus: function(a, b) {
     return a + b;
@@ -6,5 +7,6 @@ const calculator = {
 };
 
 //calculator, console은 객체. plus와 log는 함수
+
 const plus = calculator.plus(5, 5);
 console.log(plus);
