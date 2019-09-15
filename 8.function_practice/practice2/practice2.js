e3e3dfc90cb6de932544700ff914c1b3c66ec20b
MC <dhy04057@likelion.org>
2019-09-15 21:27:40 +0900

    practice2 modified

--- 8.function_practice/practice2/practice2.js
+++ 8.function_practice/practice2/practice2.js
@@ -1,18 +1,20 @@
 const title = document.querySelector("#title");
 
-const CLICKED_CLASS = "clicked";
+const CLICKED_CLASS = "clicked"; // css의 clicked class를 저장
 
 function handleClick() {
-  const hasClass = title.classList.contains(CLICKED_CLASS);
+  const hasClass = title.classList.contains(CLICKED_CLASS); // true or false값을 준다.
   if (hasClass) {
     title.classList.remove(CLICKED_CLASS);
   } else {
     title.classList.add(CLICKED_CLASS);
   }
-
-  //위의 6줄과 완전히 똑같음
-  //title.classList.toggle(CLICKED_CLASS);
 }
+
+// 위의 handleClick 함수와 똑같다.
+// function handleClick() {
+//   title.classList.toggle(CLICKED_CLASS);
+// }
 
 function init() {
   title.addEventListener("click", handleClick);
