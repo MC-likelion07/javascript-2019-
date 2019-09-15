738dcecc9ff1f2602d4955a898aa04c5c98305d3
MC <dhy04057@likelion.org>
2019-09-15 22:32:35 +0900

    name modified

--- 6.event/events.js
+++ 6.event/events.js
@@ -3,24 +3,23 @@
 // 이벤트 : 웹사이트에서 발생하는 것들.
 // We can intercept the event
 
-const title = document.querySelector("#title");
+// "resize"(이벤트) : 우리가 이벤트를 받기 기다리는 것(listen to event)
+// addEventListener method : javascript는 이벤트를 받기를 기다리고 있다.
+// 웹 브라우저의 창이 resize될 때 함수가 호출된다.(call the function handleResize when you needed.)
+// "resize" 뒤에는 함수이름을 적어준다.
+// handleResize()라고 쓰면 해당 함수가 자동적으로 호출된다.(바로 실행된다.)
+// 단, handleResize라고 쓰면 윈도우 사이즈가 변경될 때 해당 함수를 호출한다.
 
 function handleResize() {
   console.log("I have been resized");
 }
 
-// "resize" : 우리가 이벤트를 받기 기다리는 것(listen to event) javascript는 이벤트를 받기를 기다리고 있다.
-// "resize" 뒤에는 함수이름을 적어준다.
-// call the function handleResize when you needed.
-// handleResize()라고 쓰면 해당 함수가 자동적으로 호출된다.(바로 실행된다.)
-// 단, handleResize라고 쓰면 윈도우 사이즈가 변경될 때 해당 함수를 호출한다.
-
 window.addEventListener("resize", handleResize);
 
 //=================================================================================================
 // 아래의 event는 자바스크립트로부터 온 것이다.
-// 이벤트를 다룰 함수를 만들 때마다 자바스크립트는 자동적으로 함수를 객체에 붙인다.
-// event가 발생할때마다 이벤트의 객체가 호출된다.
+// event가 발생할때마다 이벤트 객체가 호출된다.
+// Event {isTrusted:true, type:"resize", target:"Window", currentTarget:Window ...}
 
 function handleResize(event) {
   console.log(event);
